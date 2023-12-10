6b36086f929947ade7dde82652078ddb5213c818
foxytouxxx <143593532+foxytouxxx@users.noreply.github.com>
2023-12-10 15:30:32 +0100

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Set the default password (CHANGE THIS TO A SECURE PASSWORD)
+GOTTY_PASSWORD=${GOTTY_PASSWORD:-mypassword}
 
-/usr/local/bin/gotty --permit-write --reconnect /bin/bash
+/usr/local/bin/gotty --permit-write --reconnect --credential $GOTTY_PASSWORD /bin/bash
