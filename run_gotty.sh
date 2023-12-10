225a2d77b4ec7536f8c5eb1365dd4473297954a0
foxytouxxx <143593532+foxytouxxx@users.noreply.github.com>
2023-12-10 15:57:55 +0100

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-set -x  # Enable debugging output
 
-# Set the default username and password for testing
-GOTTY_USERNAME=${GOTTY_USERNAME:-root}
-GOTTY_PASSWORD=${GOTTY_PASSWORD:-1234}
-
-# Create a credentials file
-echo "$GOTTY_USERNAME:$GOTTY_PASSWORD" > /tmp/gotty-credentials
-
-# Run gotty with the credentials file
-/usr/local/bin/gotty --permit-write --reconnect -c /tmp/gotty-credentials /bin/bash
-
-# Clean up the credentials file
-rm -f /tmp/gotty-credentials
+/usr/local/bin/gotty --permit-write --reconnect /bin/bash
